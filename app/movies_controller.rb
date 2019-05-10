91e7bf6ff53f08d50fb34b14cbfa130de0cd0e82
NaKia Whitby <whitby.nakia@gmail.com>
2019-05-10 17:07:28 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,7 +23,7 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args = __)
+def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
@@ -32,7 +32,7 @@ def can_be_created_in_a_block(args = __)
     m.title = args[:title]
     m.release_date = args[:release_date]
     m.save
-  end	  
+  end
 end
 
 def can_get_the_first_item_in_the_database
